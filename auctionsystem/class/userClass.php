f02775104a936bbdb50b03eb9344c37a0c487041
Anurag Pal <55744578+Anuragtech02@users.noreply.github.com>
2019-11-11 00:00:20 +0530

    Updated userClass

--- auctionsystem/class/userClass.php
+++ auctionsystem/class/userClass.php
@@ -29,37 +29,37 @@ class userClass
      public function userRegistration($username,$password,$email,$name)
      {
           try{
-          $db = getDB();
-          $st = $db->prepare("SELECT uid FROM users WHERE username=:username OR email=:email");  
-          $st->bindParam("username", $username,PDO::PARAM_STR);
-          $st->bindParam("email", $email,PDO::PARAM_STR);
-          $st->execute();
-          $count=$st->rowCount();
+			  $db = getDB();
+			  $st = $db->prepare("SELECT uid FROM users WHERE username=:username OR email=:email");  
+			  $st->bindParam("username", $username,PDO::PARAM_STR);
+			  $st->bindParam("email", $email,PDO::PARAM_STR);
+			  $st->execute();
+			  $count=$st->rowCount();
           if($count<1)
           {
-          $stmt = $db->prepare("INSERT INTO users(username,password,email,name) VALUES (:username,:hash_password,:email,:name)");  
-          $stmt->bindParam("username", $username,PDO::PARAM_STR) ;
-          $hash_password= hash('sha256', $password);
-          $stmt->bindParam("hash_password", $hash_password,PDO::PARAM_STR) ;
-          $stmt->bindParam("email", $email,PDO::PARAM_STR) ;
-          $stmt->bindParam("name", $name,PDO::PARAM_STR) ;
-          $stmt->execute();
-          $uid=$db->lastInsertId();
-          $db = null;
-          $_SESSION['uid']=$uid;
-          return true;
+			  $stmt = $db->prepare("INSERT INTO users(username,password,email,name) VALUES (:username,:hash_password,:email,:name)");  
+			  $stmt->bindParam("username", $username,PDO::PARAM_STR) ;
+			  $hash_password= hash('sha256', $password);
+			  $stmt->bindParam("hash_password", $hash_password,PDO::PARAM_STR) ;
+			  $stmt->bindParam("email", $email,PDO::PARAM_STR) ;
+			  $stmt->bindParam("name", $name,PDO::PARAM_STR) ;
+			  $stmt->execute();
+			  $uid=$db->lastInsertId();
+			  $db = null;
+			  $_SESSION['uid']=$uid;
+			  return true;
 
           }
           else
           {
-          $db = null;
-          return false;
+			  $db = null;
+			  return false;
           }
           
          
           } 
           catch(PDOException $e) {
-          echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+			echo '{"error":{"text":'. $e->getMessage() .'}}'; 
           }
      }
      
@@ -68,7 +68,7 @@ class userClass
      {
         try{
           $db = getDB();
-          $stmt = $db->prepare("SELECT email,username,name FROM users WHERE uid=:uid");  
+          $stmt = $db->prepare("SELECT email,username,name,uid FROM users WHERE uid=:uid");  
           $stmt->bindParam("uid", $uid,PDO::PARAM_INT);
           $stmt->execute();
           $data = $stmt->fetch(PDO::FETCH_OBJ);
@@ -79,7 +79,65 @@ class userClass
           }
 
      }
+	 /*Adds item to database*/
+	 public function addItem($uid, $item_name, $item_min_bid)
+	 {
+		try{
+			$db = getDB();
+			$stmt = $db->prepare("INSERT INTO items (uid,item_name,item_min_bid) values (:uid,:item_name,:item_min_bid)");
+			$stmt->bindParam("uid", $uid,PDO::PARAM_INT);
+			$stmt->bindParam("item_name", $item_name,PDO::PARAM_STR);
+			$stmt->bindParam("item_min_bid", $item_min_bid, PDO::PARAM_INT);
+			$stmt->execute();
+			$item_id=$db->lastInsertId();
+			$db=null;
+			$_SESSION['uid']=$uid;
+			return true;
+		}	
+		catch(PDOException $e){
+			$db = null;
+			echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+		}
+		 
+	 }
+	 
+	 public function removeItem($uid, $item_name)
+	 {
+		 
+		 try{
+			 $db=getDB();
+			 $stmt=$db->prepare("DELETE FROM items WHERE uid=:uid and item_name=:item_name");
+			 $stmt->bindParam("uid", $uid,PDO::PARAM_INT);
+			 $stmt->bindParam("item_name", $item_name,PDO::PARAM_STR);
+			 $stmt->execute();
+			 $db=null;
+			 $_SESSION['uid']=$uid;
+			 return true;
+		 }
+		 catch(PDOException $e){
+			 $db = null;
+			 echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+		 }
+	 }
 
+	 public function bid($uid,$item_name, $your_bid)
+	 {
+		 try{
+			 $db=getDB();
+			 $stmt=$db->prepare("UPDATE items SET bid_id=:uid,your_bid=:your_bid WHERE item_name=:item_name");
+			 $stmt->bindParam("your_bid", $your_bid, PDO::PARAM_INT);
+			 $stmt->bindParam("uid", $uid, PDO::PARAM_INT);
+			 $stmt->bindParam("item_name", $item_name, PDO::PARAM_STR);
+			 $stmt->execute();
+			 $db=null;
+			 $_SESSION['uid']=$uid;
+			 return true;
+		 }
+		 catch(PDOException $e){
+			 $db=null;
+			 echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+		 }
+	 }
 
 }
 ?>
